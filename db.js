95d02bdc3b05f3bdb6c826add4cc015c6699cbe2
BogKowit <BogKowit@gmail.com>
2022-08-30 08:12:54 +0200

    ++

--- db.js
+++ db.js
@@ -1,23 +1,12 @@
-const items = require("./data/stats");
-
+const stats = require("./data/stats");
 
 module.exports = {
   stats,
 };
 
-// const items = require("./data/items");
+// const stats = require("./data/stats");
 
 // module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
 
 //   return {
 //     stats,
